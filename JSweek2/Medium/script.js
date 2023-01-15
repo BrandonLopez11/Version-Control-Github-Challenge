894b30173eff6c3a9bb401922a969eef1a247dfe
BrandonLopez11 <114833892+BrandonLopez11@users.noreply.github.com>
2023-01-14 20:39:08 -0500

    Delete JSweek2 directory

--- JSweek2/Medium/script.js
+++
@@ -1,9 +0,0 @@
-let input = prompt("Enter some text: ");
-
-if (input == input.toUpperCase()) {
-    console.log("The user is shouting!");
-} else if (input === input.toLowerCase()) {
-    console.log("The user is whispering!");
-} else {
-    console.log("The user is talking normally.");
-}
--- JSweek2/VeryEasy/script.js
+++
@@ -1,5 +0,0 @@
-const x = 6;
-
-const z = 2;
-
-console.log(`The difference between ${x} and ${z} is ${x - z}`);
